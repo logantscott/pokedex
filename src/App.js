1997b25d4e0f00876b4553f5f008fefd3bfcfdcb
Logan Scott <logantscott@gmail.com>
2020-02-13 12:07:12 -0800

    stuff

--- src/App.js
+++ src/App.js
@@ -8,24 +8,50 @@ import PokeList from './PokeList.js';
 
 export default class App extends Component {
   state = {
-    pokedex: []
+    pokedex: [],
+    searchString: null
   }
 
   // https://alchemy-pokedex.herokuapp.com
 
+  handleTextUpdate = e => {
+    this.setState({ searchString: e.target.value });
+  };
+
   async componentDidMount() {
-    const query = 'https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=17';
+    let query = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
     const data = await request.get(query)
 
-    this.setState({pokedex: data.body.results});
-    console.log(this.state.pokedex)
+    this.setState({pokedex: data.body});
+  }
+
+handleSearch = async() => {
+    let query = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+    query = this.state.searchString ? query + '?pokemon=' + this.state.searchString : query;
+    const data = await request.get(query);
+
+    this.setState({pokedex: data.body});
   }
 
   render() {
+
+    // const filtered = (this.state.pokedex.results ? this.state.pokedex.results : [])
+    // .filter(pokemon => {
+    //   if (!this.state.searchString) return true;
+
+    //   return (pokemon.pokemon.toLowerCase().includes(this.state.searchString.toLowerCase())
+    // )});
+
+
+
     return (
       <div className="App">
         <Header image={pokedexImg} />
-        <PokeList pokedex={this.state.pokedex} />
+        <label> Search: </label>
+        <input type="text" className="searchtext" onChange={this.handleTextUpdate}>
+        </input>
+        <button onClick={this.handleSearch}>Search</button>
+        <PokeList pokedex={this.state.pokedex.results ? this.state.pokedex.results : []} />
       </div>
     );
   }
--- src/PokeItem.js
+++ src/PokeItem.js
@@ -5,8 +5,16 @@ export default class PokeItem extends Component {
   render() {
     return (
       <li className="grid-item" style={{borderColor:this.props.pokemon.color_1}}>
-            <img src={this.props.pokemon.url_image} alt={this.props.pokemon.pokemon} />
             <h3>{this.props.pokemon.pokemon}</h3>
+            <img src={this.props.pokemon.url_image} alt={this.props.pokemon.pokemon} />
+            <p>
+                <span className="bodydetail">
+                    height: {this.props.pokemon.height}
+                </span>
+                <span className="bodydetail">
+                    weight: {(this.props.pokemon.weight / 10).toFixed(1)} kg
+                </span>
+            </p>
             {/* <p>Types:</p> */}
             <p>
                 <span style={{backgroundColor:this.props.pokemon.color_1 === 'NA' ? 'transparent' : this.props.pokemon.color_1}}>
@@ -16,6 +24,28 @@ export default class PokeItem extends Component {
                     {this.props.pokemon.type_2 === 'NA' ? '' : this.props.pokemon.type_2}
                 </span>
             </p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>hp</th>
+                        <th>atk</th>
+                        <th>def</th>
+                        <th>sp atk</th>
+                        <th>sp def</th>
+                        <th>speed</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{this.props.pokemon.hp}</td>
+                        <td>{this.props.pokemon.attack}</td>
+                        <td>{this.props.pokemon.defense}</td>
+                        <td>{this.props.pokemon.special_attack}</td>
+                        <td>{this.props.pokemon.special_defense}</td>
+                        <td>{this.props.pokemon.speed}</td>
+                    </tr>
+                </tbody>
+            </table>
       </li>
     );
   }
